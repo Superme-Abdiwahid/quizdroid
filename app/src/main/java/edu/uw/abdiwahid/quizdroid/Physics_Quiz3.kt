e9839be3878bae2a1262c1dab3152ab760eee897
Abdiwahid <ahajir@uw.edu>
2022-02-04 15:29:40 -0800

    More changes

--- app/src/main/java/edu/uw/abdiwahid/quizdroid/Physics_Quiz3.kt
+++ app/src/main/java/edu/uw/abdiwahid/quizdroid/Physics_Quiz3.kt
@@ -6,47 +6,50 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.RadioButton
 
-class Physics_Quiz2 : AppCompatActivity() {
+class Physics_Quiz3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_physics_quiz2)
+        setContentView(R.layout.activity_physics_quiz3)
+
         var submit_button: Button = findViewById<Button>(R.id.submit_button)
 
         submit_button.isEnabled = false;
 
         var answer_a: RadioButton = findViewById<RadioButton>(R.id.answer_a)
-//
+
         var answer_b: RadioButton = findViewById<RadioButton>(R.id.answer_b)
-////
+
         var answer_c: RadioButton = findViewById<RadioButton>(R.id.answer_c)
-////
+
         var answer_d: RadioButton = findViewById<RadioButton>(R.id.answer_d)
-//
-//
-//        var go_back: Button = findViewById<Button>(R.id.go_back)
-//
-//
-        answer_a.setOnClickListener(){
+
+
+        var go_back: Button = findViewById<Button>(R.id.go_back)
+
+
+        answer_a.setOnClickListener() {
             submit_button.isEnabled = true;
         }
-//
-        answer_b.setOnClickListener(){
-            submit_button.isEnabled = true;
 
-        }
-
-        answer_c.setOnClickListener(){
+        answer_b.setOnClickListener() {
             submit_button.isEnabled = true;
 
         }
 
-        answer_d.setOnClickListener(){
+        answer_c.setOnClickListener() {
             submit_button.isEnabled = true;
 
         }
 
-        submit_button.setOnClickListener(){
-            var intent = Intent(this@Physics_Quiz2, Physics_Quiz3::class.java)
+        answer_d.setOnClickListener() {
+            submit_button.isEnabled = true;
+
+        }
+
+
+
+        go_back.setOnClickListener() {
+            val intent = Intent(this@Physics_Quiz3, MainActivity::class.java)
             startActivity(intent)
         }
     }
