6811f3f77b4a93788148e4309b885df023b1e6dd
Abdiwahid <ahajir@uw.edu>
2022-02-09 11:41:14 -0800

    Finished Homework part 2

--- app/src/main/java/edu/uw/abdiwahid/quizdroid/Answers.kt
+++ app/src/main/java/edu/uw/abdiwahid/quizdroid/Answers.kt
@@ -4,39 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import androidx.annotation.BoolRes
 import java.util.*
 import kotlin.collections.HashMap
 
-class MainActivity : AppCompatActivity() {
+class Answers : AppCompatActivity() {
+    var read = arrayListOf<QuizTopics>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-        var math_quiz: Button = findViewById<Button>(R.id.math_quiz)
-
-        var physics_Quiz: Button = findViewById<Button>(R.id.physics_quiz)
-
-
-        var super_hero_Quiz: Button = findViewById<Button>(R.id.super_hero_quiz)
-
-
-        math_quiz.setOnClickListener(){
-            val intent = Intent(this@MainActivity, Quiz_Overview::class.java)// first argument is the context
-            startActivity(intent)  // send the intent
-        }
-
-        physics_Quiz.setOnClickListener(){
-            val intent = Intent(this@MainActivity, Physics_Quiz::class.java)// first argument is the context
-            startActivity(intent)  // send the intent
-        }
-
-        super_hero_Quiz.setOnClickListener(){
-            val intent = Intent(this@MainActivity, Super_Hero_Quiz::class.java)// first argument is the context
-            startActivity(intent)  // send the intent
-        }
+        setContentView(R.layout.activity_answers)
     }
+
     @SuppressWarnings("depcreated")
     interface Repositry{
         fun addQuiz(topic: QuizTopics)
@@ -53,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         var read = LinkedList<QuizTopics>()
         var map = HashMap<String, String>();
         override fun addQuiz(topic: QuizTopics) {
-            read.add(topic)
+                                                                                                                                                                                                                                                        read.add(topic)
             read.addAll(read);
             Log.d("MAIN_ACTIVITY", read.toString())
             if(!map.containsKey("MAIN_ACTIVITY")){
